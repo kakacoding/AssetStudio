984d394da349bc88add5513604be9f77a54f9c32
kakacoding <kyo_siye@163.com>
2024-06-14 16:18:15 +0800

    AssetStudio库缓存m_WrapV和m_WrapW

--- AssetStudio/Classes/Texture2D.cs
+++ AssetStudio/Classes/Texture2D.cs
@@ -31,6 +31,8 @@ namespace AssetStudio
         public int m_Aniso;
         public float m_MipBias;
         public int m_WrapMode;
+        public int m_WrapV;
+        public int m_WrapW;
 
         public GLTextureSettings(ObjectReader reader)
         {
@@ -42,8 +44,8 @@ namespace AssetStudio
             if (version[0] >= 2017)//2017.x and up
             {
                 m_WrapMode = reader.ReadInt32(); //m_WrapU
-                int m_WrapV = reader.ReadInt32();
-                int m_WrapW = reader.ReadInt32();
+                m_WrapV = reader.ReadInt32();
+                m_WrapW = reader.ReadInt32();
             }
             else
             {
